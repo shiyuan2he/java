c78df17de8d5f123dc03e61c1b933b64ae67f2a9
shiyuan2he <shiyuan4work@sina.com>
2017-03-04 23:20:56 +0800

    多线程编程项目搭建

---
+++ javase/javase-thread/src/main/java/com/javase/thread/MyThread.java
@@ -0,0 +1,21 @@
+package com.javase.thread;
+/**
+ * @description <p>继承Thread的线程</p>
+ * @path javase-thread/com.javase.thread
+ * @time 2017年3月4日 下午11:12:50
+ * @github 
+ *     http://github.com/shiyuan2he
+ * @author shiyuan2he
+ * @email shiyuan4work@sina.com
+ * Copyright (c) 2016 shiyuan4work@sina.com All rights reserved
+ */
+public class MyThread extends Thread{
+	/**
+	 * <p></p>
+	 */
+	@Override
+	public void run() {
+		
+	}
+}
+
