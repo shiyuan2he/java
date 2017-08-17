1d9a69b23b6b68df6d68db164c1f2173600de5d8
shiyuan2he <shiyuan4work@sina.com>
2017-08-17 18:31:19 +0800

    添加日志插件slf4j+logback

---
+++ javase/javase-common/src/test/java/com/hsy/javase/utils/CollectionHelperTest.java
@@ -0,0 +1,14 @@
+package com.hsy.javase.utils;
+
+/**
+ * @author heshiyuan
+ * @description <p></p>
+ * @path java/com.hsy.javase.utils
+ * @date 17/08/2017 11:20 AM
+ * @github http://github.com/shiyuan2he
+ * @email shiyuan4work@sina.com
+ * Copyright (c) 2017 shiyuan4work@sina.com All rights reserved.
+ * @price ¥5    微信：hewei1109
+ */
+public class CollectionHelperTest {
+}
