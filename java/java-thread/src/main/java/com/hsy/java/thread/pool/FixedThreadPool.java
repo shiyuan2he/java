8e95c812012344d2ce1cbeab4314d7784fdff9c6
shiyuan2he <shiyuan4work@sina.com>
2017-09-23 18:08:34 +0800

    添加线程池模块，支持多线程开发

---
+++ java/java-thread/src/main/java/com/hsy/java/thread/pool/FixedThreadPool.java
@@ -0,0 +1,41 @@
+package com.hsy.java.thread.pool;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author heshiyuan
+ * @description <p>
+ *      可重用固定线程数的线程池
+ *      以共享的无界队列方式来运行这些线程。在任意点，
+ *      在大多数 nThreads 线程会处于处理任务的活动状态。
+ *      如果在所有线程处于活动状态时提交附加任务，则在有可用线程之前，
+ *      附加任务将在队列中等待。如果在关闭前的执行期间由于失败而导致任何线程终止，
+ *      那么一个新线程将代替它执行后续的任务（如果需要）。在某个线程被显式地关闭之前，
+ *      池中的线程将一直存在。
+ *      </p>
+ * @path framework/com.hsy.thread.pool
+ * @date 23/08/2017 5:01 PM
+ * @github http://github.com/shiyuan2he
+ * @email shiyuan4work@sina.com
+ * Copyright (c) 2017 shiyuan4work@sina.com All rights reserved.
+ * @price ¥5    微信：hewei1109
+ */
+public class FixedThreadPool {
+
+    private final ExecutorService fixedThreadPool;
+
+    public FixedThreadPool(int threadPoolNum) {
+        this.fixedThreadPool = Executors.newFixedThreadPool(threadPoolNum,new ThreadFactory() {
+            AtomicInteger atomic = new AtomicInteger();
+            public Thread newThread(Runnable runnable) {
+                return new Thread(runnable, "fixedThreadPool-" + this.atomic.getAndIncrement());
+            }
+        });
+    }
+    public ExecutorService getFixedThreadPool() {
+        return fixedThreadPool;
+    }
+}
