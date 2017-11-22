504005e11bffb516241d11f64abc9c4335d8baa7
shiyuan2he <shiyuan4work@sina.com>
2017-11-22 22:44:48 +0800

    build

--- java/java-util/src/test/java/com/hsy/java/java/util/secure/Base64HelperTest.java
+++ java/java-util/src/test/java/com/hsy/java/java/util/secure/Base64HelperTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.hsy.java.util.secure.Base64Helper;
+
 /**
  * @author heshiyuan
  * @description <p></p>
