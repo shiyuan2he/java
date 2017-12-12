742f30a0420e2680bd08f3080eed8b2a9c27c80a
shiyuan2he <shiyuan4work@sina.com>
2017-12-12 18:01:17 +0800

    java项目重构

--- java/java-base/src/test/java/com/hsy/java/java/base/array/Set2MapTest.java
+++ java/java-base/src/test/java/com/hsy/java/java/base/array/Set2MapTest.java
@@ -17,11 +17,11 @@ public class Set2MapTest {
 		// 访问Map集合里包含key-value对
 		System.out.println(scores.size());
 		scores.removeEntry("数学");
-		System.out.println("删除key为\"数学\"的Entry之后：" + scores);
+		System.out.println("删除key为数学的Entry之后：" + scores);
 		// 根据key取出value
 		System.out.println("语文成绩：" + scores.get("语文"));
 		// 判断是否包含指定key
-		System.out.println("是否包含\"英文\"key :" + scores.containsKey("英文"));
+		System.out.println("是否包含英文key :" + scores.containsKey("英文"));
 		// 判断是否包含指定value
 		System.out.println("是否包含 82 value :" + scores.containsValue(82));
 		// 清空集合
