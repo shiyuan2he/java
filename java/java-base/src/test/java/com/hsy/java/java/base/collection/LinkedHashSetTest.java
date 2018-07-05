84741a9c476c92fdf27198cce12951891e25ac35
shiyuan2he <shiyuan4work@sina.com>
2018-07-05 18:17:07 +0800

    jdk源码剖析

---
+++ java/java-base/src/test/java/com/hsy/java/java/base/collection/LinkedHashSetTest.java
@@ -0,0 +1,71 @@
+package com.hsy.java.java.base.collection;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.*;
+
+/**
+ * @author heshiyuan
+ * @description <p></p>
+ * @path java/com.hsy.java.java.base.collection
+ * @date 2018/7/5 13:35
+ * @github http://github.com/shiyuan2he
+ * @email shiyuan4work@sina.com
+ * Copyright (c) 2018 shiyuan4work@sina.com All rights reserved.
+ * @price ¥5    微信：hewei1109
+ */
+public class LinkedHashSetTest {
+    private Logger logger = LoggerFactory.getLogger(getClass());
+    @Test
+    public void testLinkedHashSet1(){
+        Set<Integer> sets = new LinkedHashSet<>(20);
+        for (int i = 0; i < 10; i++) {
+            int count = new Random().nextInt(10);
+            sets.add(count);
+            logger.info("{}",count);
+        }
+        logger.info("{}",sets.toString());
+    }
+    @Test
+    public void testTreeSet2(){
+        Set<User> sets = new LinkedHashSet<>();
+        for (int i = 0; i < 10; i++) {
+            int count = new Random().nextInt(10);
+            logger.info("{}",count);
+            User user = new User();
+            user.setId(count);
+            sets.add(user);
+        }
+        logger.info("{}",sets.toString());
+    }
+    class User{
+        private Integer id;
+        private String name;
+
+        public Integer getId() {
+            return id;
+        }
+
+        public void setId(Integer id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return "User{" +
+                    "id=" + id +
+                    ", name='" + name + '\'' +
+                    '}';
+        }
+    }
+}
