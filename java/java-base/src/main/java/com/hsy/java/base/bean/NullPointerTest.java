c1b631967b2330a60be1c479e8a775ed3f94fc2e
shiyuan2he <shiyuan4work@126.com>
2018-10-18 20:56:40 +0800

    工具类添加

---
+++ java/java-base/src/main/java/com/hsy/java/base/bean/NullPointerTest.java
@@ -0,0 +1,23 @@
+package com.hsy.java.base.bean;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author heshiyuan
+ * @description <p></p>
+ * @path java/com.hsy.java.base.bean
+ * @date 17/10/2018 7:01 PM
+ * @github http://github.com/shiyuan2he
+ * @email shiyuan4work@126.com
+ * Copyright (c) 2018 shiyuan4work@sina.com All rights reserved.
+ * @price ¥5    微信：hewei1109
+ */
+public class NullPointerTest {
+
+
+    /*public static void main(String[] args) {
+        String idCard = NullPointerTest.of().getIdCard();
+        System.out.println(idCard);
+    }*/
+}
--- java/java-base/src/main/java/com/hsy/java/base/date/DateHelper.java
+++ java/java-base/src/main/java/com/hsy/java/base/date/DateHelper.java
@@ -7,8 +7,14 @@
  */
 package com.hsy.java.base.date;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -98,6 +104,137 @@ public class DateHelper {
 	    formatter = new SimpleDateFormat ("yyyy-MM-dd HH:mm:ss"); 
 	    String ctime = formatter.format(time); 
 	    return ctime; 
-	} 
+	}
+
+    public static final String DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
+    public static final String SIMPLE_DAY_FORMAT = "yyyyMMdd";
+    public static final Long MILLIS_OF_ONE_YEAR = 31536000000L;
+
+
+    public static void main(String[] args) {
+        //2分钟后
+        System.out.println(now() + 1000 * 60 * 2);
+
+        //4分钟后
+        System.out.println(now() + 1000 * 60 * 4);
+
+        //6分钟后
+        System.out.println(now() + 1000 * 60 * 6);
+
+        //8分钟后
+        System.out.println(now() + 1000 * 60 * 8);
+    }
+
+    /**
+     * 获取当前UTC时间
+     *
+     * @return 返回UTC时间毫秒
+     */
+    public static long now() {
+        Calendar cal = Calendar.getInstance();
+        int zoneOffset = cal.get(Calendar.ZONE_OFFSET);
+        int dstOffset = cal.get(Calendar.DST_OFFSET);
+        cal.add(Calendar.MILLISECOND, -(zoneOffset + dstOffset));
+        return cal.getTimeInMillis();
+    }
+
+
+    public static String nowStr() {
+        Calendar cal = Calendar.getInstance();
+        int zoneOffset = cal.get(Calendar.ZONE_OFFSET);
+        int dstOffset = cal.get(Calendar.DST_OFFSET);
+        cal.add(Calendar.MILLISECOND, -(zoneOffset + dstOffset));
+        SimpleDateFormat formatter = new SimpleDateFormat(DATE_FORMAT);
+        return formatter.format(cal.getTime());
+    }
+
+    public static String now(String format) {
+        SimpleDateFormat formatter = new SimpleDateFormat(format);
+        return formatter.format(new Date(now()));
+    }
+
+    public static int nowYearMonthDay() {
+        Calendar cal = Calendar.getInstance();
+        int zoneOffset = cal.get(Calendar.ZONE_OFFSET);
+        int dstOffset = cal.get(Calendar.DST_OFFSET);
+        cal.add(Calendar.MILLISECOND, -(zoneOffset + dstOffset));
+        SimpleDateFormat formatter = new SimpleDateFormat(SIMPLE_DAY_FORMAT);
+        return Integer.parseInt(formatter.format(cal.getTime()));
+    }
+
+    /**
+     * 获取当前UTC时间
+     *
+     * @return 返UTC秒
+     */
+    public static long currentTime() {
+        Calendar cal = Calendar.getInstance();
+        int zoneOffset = cal.get(Calendar.ZONE_OFFSET);
+        int dstOffset = cal.get(Calendar.DST_OFFSET);
+        cal.add(Calendar.MILLISECOND, -(zoneOffset + dstOffset));
+        return cal.getTimeInMillis() / 1000;
+    }
+
+
+    public static LocalDate date2LocalDate(Date date) {
+        Instant instant = date.toInstant();
+        ZonedDateTime zdt = instant.atZone(ZoneId.systemDefault());
+        return zdt.toLocalDate();
+    }
+
+
+    public static Long getTimeMilliseconds(String time) {
+        if (!StringUtils.isNotBlank(time)) {
+            return now();
+        }
+        SimpleDateFormat formatter = new SimpleDateFormat(DATE_FORMAT);
+        try {
+            Calendar cal = Calendar.getInstance();
+            cal.setTime(formatter.parse(time));
+            int zoneOffset = cal.get(Calendar.ZONE_OFFSET);
+            int dstOffset = cal.get(Calendar.DST_OFFSET);
+            cal.add(Calendar.MILLISECOND, -(zoneOffset + dstOffset));
+            return cal.getTimeInMillis();
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static String fromMilliseconds(Long milliseconds) {
+        if (milliseconds == null || milliseconds == 0) {
+            milliseconds = now();
+        }
+        SimpleDateFormat formatter = new SimpleDateFormat(DATE_FORMAT);
+        Date date = new Date(milliseconds);
+        return formatter.format(date);
+    }
+
+
+    public static String fromMilliseconds(long milliseconds) {
+        if (milliseconds == 0) {
+            milliseconds = now();
+        }
+        SimpleDateFormat formatter = new SimpleDateFormat(DATE_FORMAT);
+        Date date = new Date(milliseconds);
+        return formatter.format(date);
+    }
+
+
+    public static long simpleDay2Milliseconds(int day) {
+        SimpleDateFormat formatter = new SimpleDateFormat(SIMPLE_DAY_FORMAT);
+        try {
+            return formatter.parse(String.valueOf(day)).getTime();
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return -1;
+        }
+    }
+
+    public static int milliseconds2SimpleDay(long milliseconds) {
+        Date date = new Date(milliseconds);
+        SimpleDateFormat formatter = new SimpleDateFormat(SIMPLE_DAY_FORMAT);
+        return Integer.parseInt(formatter.format(date));
+    }
 }
 
